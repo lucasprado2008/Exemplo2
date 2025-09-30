b5001f39adcbdd7162a048050bed6ded6b504c8b
Lucas <lucashenriquecardosoprado@gmail.com>
2025-09-30 16:04:46 -0300

    Criando arquivos, base da página inicial, adicionando imagens e colocando ícone da página

---
+++ Exemplo2/Controllers/CatalogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exemplo2.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Exemplo2/Controllers/HomeController.cs
+++ Exemplo2/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Exemplo2.Controllers
             return View();
         }
 
+        public IActionResult Catalogo()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
